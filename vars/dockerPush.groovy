4a7f6be6133f0434343c8dd42ae3fc37dc1ee7cd
shubham Sadashiv mali <149328245+Shubhamx18@users.noreply.github.com>
2026-02-15 17:00:48 +0530

    DockerHub-Push-Image

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -2,7 +2,7 @@ def call(String imageName) {
 
     sh """
     docker tag ${imageName} \$USER/${imageName}
-    docker push \$USER/${imageName}
+    docker push \$USER/${imageName} 
     docker logout
     """
 }
